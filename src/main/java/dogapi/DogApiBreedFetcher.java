5d1474097079c6117dd7a2f126d070e06e88be02
majinbagel <aidan.wangzh@gmail.com>
2025-10-19 21:41:11 -0400

    got rid of redundant client

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -16,7 +16,6 @@ import java.util.*;
  * exceptions to align with the requirements of the BreedFetcher interface.
  */
 public class DogApiBreedFetcher implements BreedFetcher {
-    private final OkHttpClient client = new OkHttpClient();
 
     /**
      * Fetch the list of sub breeds for the given breed from the dog.ceo API.
@@ -26,10 +25,6 @@ public class DogApiBreedFetcher implements BreedFetcher {
      */
     @Override
     public List<String> getSubBreeds(String breed) {
-        // TODO Task 1: Complete this method based on its provided documentation
-        //      and the documentation for the dog.ceo API. You may find it helpful
-        //      to refer to the examples of using OkHttpClient from the last lab,
-        //      as well as the code for parsing JSON responses.
         // return statement included so that the starter code can compile and run.
         final OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
